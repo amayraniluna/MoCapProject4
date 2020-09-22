515f5fbbf18a55a8d3ee0c830cfc24f559024069
amayraniluna <asluna@smu.edu>
2020-09-22 16:34:34 -0500

    Squares update

--- xcode/Squares.h
+++ xcode/Squares.h
@@ -44,9 +44,7 @@ public:
                int y1 = j * squareHeight;
                int y2 = y1 + squareHeight;
                Rectf curSquare = Rectf(x1, y1, x2, y2);
-               
-               /* modify code to count features rather than white pixels
-                *
+                
                int sum = 0;
                //counting white pixels
                for(int x = x1 ; x < x2 ; x++){
@@ -58,10 +56,137 @@ public:
                //coloring white pixels
                gl::color(sum/(float)(n*n*255), 0, 1, 1);
                gl::drawSolidRect(curSquare);
-                *
-                */
+            
+                
            }
         }
    }
 };
 #endif // Squares_h
+
+
+
+
+
+
+
+/*
+//
+//  Header.h
+//
+//
+//  Created by courtney on 9/17/20.
+//
+
+#ifndef Header_h
+#define Header_h
+
+class Squares
+{
+protected:
+    int N;
+    virtual float count(ci::Rectf)=0;
+    virtual float getDivisorOfSum()=0;
+    void setN(int n){N=n;}
+
+
+    virtual void drawRect()
+    {
+        int squareWidth = frameDiff.cols / n;
+        int squareHeight = frameDiff.rows / n;
+        ci::Rectf curSquare;
+        
+        
+        //creating squares
+        for(int i = 0 ; i < n ; i++){
+            for(int j = 0 ; j < n ; j++){
+                int x1 = i * squareWidth;
+                int x2 = x1 + squareWidth;
+                int y1 = j * squareHeight;
+                int y2 = y1 + squareHeight;
+                Rectf curSquare = Rectf(x1, y1, x2, y2);
+
+                float sum = count(curSquare);
+            
+                gl::color(sum/getDivisorOfSum(), 0 ,1 , 1);
+                gl::drawSolidRect(curSquare);
+            }
+        }
+        //do all the things to find the squares -
+        
+
+        
+        
+        //divide sum by the appropriate numbers
+        //use the result from above to change color
+        //draw squares
+    }
+    
+
+};
+
+
+
+class SquaresFrameDiff : public Squares
+{
+private:
+    cv::Mat frameDiff;
+public:
+    virtual void drawRect(cv::Mat b)
+    {
+        frameDiff = b;
+        Squares::drawRect();
+        
+    }
+    virtual float count(ci::Rectf curSquare)
+    {
+        //return the sum from the framedifferencing
+        return 0;
+    }
+    
+    float getDivisorOfSum()
+    {
+        return 1;
+    }
+    
+
+};
+
+class SquaresFeatures : public Squares
+{
+    private:
+    std::vector<cv::Point2f> features;
+    
+  public:
+    virtual void drawRect(std::vector<cv::Point2f> pts)
+    {
+        features = pts;
+    //get features size in class variable
+        Squares::drawRect();
+        
+    }
+
+    virtual float count(ci::Rectf curSquare)
+    {   sum = 0;
+        //return the sum of the features points
+        for(int i = 0 ; i < features.size(); i++){
+            if(curSquare.contains(fromOcv(features[i])){
+                sum++;
+            }
+        }
+        return sum;
+    }
+    
+    float getDivisorOfSum()
+    {
+        return 1; //return features size class variable 
+    }
+    
+};
+
+
+
+
+
+#endif /* Header_h*/
+ 
