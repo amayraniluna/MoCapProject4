9369bd4f65eefe0be748fbb96818685bddba03d1
amayraniluna <asluna@smu.edu>
2020-09-22 16:55:09 -0500

    Squares inheritance added

--- xcode/Squares.h
+++ xcode/Squares.h
@@ -16,51 +16,104 @@
 using namespace cinder;
 
 class squares{
-private:
-    int n;
-    
-public:
-    
-    //constructor
-    squares(){
-        n = 10;
-    }
- 
-    void setN(int n1){
-        n = n1;
-    }
-   
-   //divides screen into n*n squares and colors white pixels that indicate movement
-   void draw(cv::Mat frameDiff)
-   {
-       int squareWidth = frameDiff.cols / n;
-       int squareHeight = frameDiff.rows / n;
-       
-       //creating squares
-       for(int i = 0 ; i < n ; i++){
-           for(int j = 0 ; j < n ; j++){
-               int x1 = i * squareWidth;
-               int x2 = x1 + squareWidth;
-               int y1 = j * squareHeight;
-               int y2 = y1 + squareHeight;
-               Rectf curSquare = Rectf(x1, y1, x2, y2);
-                
-               int sum = 0;
-               //counting white pixels
-               for(int x = x1 ; x < x2 ; x++){
-                   for(int y = y1 ; y < y2 ; y++){
-                       int pixel = frameDiff.at<uint8_t>(y,x);
-                       sum+=pixel;
-                   }
-               }
-               //coloring white pixels
-               gl::color(sum/(float)(n*n*255), 0, 1, 1);
-               gl::drawSolidRect(curSquare);
-            
-                
-           }
+    protected:
+    int N;
+    virtual float count(ci::Rectf)=0;
+    virtual float getDivisorOfSum()=0;
+    void setN(int n){N=n;}
+
+
+    virtual void drawRect()
+    {
+        int squareWidth = frameDiff.cols / N;
+        int squareHeight = frameDiff.rows / N;
+        ci::Rectf curSquare;
+        
+        //creating squares
+        for(int i = 0 ; i < N ; i++){
+            for(int j = 0 ; j < N ; j++){
+                int x1 = i * squareWidth;
+                int x2 = x1 + squareWidth;
+                int y1 = j * squareHeight;
+                int y2 = y1 + squareHeight;
+                Rectf curSquare = Rectf(x1, y1, x2, y2);
+
+                float sum = count(curSquare);
+                //divide sum by the appropriate numbers
+                //use the result from above to change color
+                gl::color(sum/getDivisorOfSum(), 0 ,1 , 1);
+                //draw squares
+                gl::drawSolidRect(curSquare);
+            }
         }
-   }
+    }
+};
+
+
+class SquaresFrameDiff : public squares
+{
+private:
+    cv::Mat frameDiff;
+    float sum = 0.0;
+public:
+    virtual void drawRect(cv::Mat b)
+    {
+        frameDiff = b;
+        squares::drawRect();
+        
+    }
+    virtual float count(ci::Rectf curSquare)
+    {
+        //counting white pixels
+        for(int x = curSquare.x1 ; x < curSquare.x2 ; x++){
+            for(int y = curSquare.y1 ; y < curSquare.y2 ; y++){
+                int pixel = frameDiff.at<uint8_t>(y,x);
+                sum+=pixel;
+            }
+        }
+        return sum;
+    }
+    
+    float getDivisorOfSum()
+    {
+        float div = N*N*255;
+        return div;
+    }
+    
+
+};
+
+class SquaresFeatures : public squares
+{
+    private:
+    std::vector<cv::Point2f> features;
+    
+  public:
+    virtual void drawRect(std::vector<cv::Point2f> pts)
+    {
+        features = pts;
+    //get features size in class variable
+        squares::drawRect();
+        
+    }
+
+    virtual float count(ci::Rectf curSquare)
+    {
+        float sum = 0.0;
+        //return the sum of the features points
+        for(int i = 0 ; i < features.size(); i++){
+            if(curSquare.contains(fromOcv(features[i]))){
+                sum++;
+            }
+        }
+        return sum;
+    }
+    
+    float getDivisorOfSum()
+    {
+        return 1; //return features size class variable
+    }
+    
 };
 #endif // Squares_h
 
@@ -96,7 +149,6 @@ protected:
         int squareHeight = frameDiff.rows / n;
         ci::Rectf curSquare;
         
-        
         //creating squares
         for(int i = 0 ; i < n ; i++){
             for(int j = 0 ; j < n ; j++){
@@ -107,22 +159,14 @@ protected:
                 Rectf curSquare = Rectf(x1, y1, x2, y2);
 
                 float sum = count(curSquare);
-            
+                //divide sum by the appropriate numbers
+                //use the result from above to change color
                 gl::color(sum/getDivisorOfSum(), 0 ,1 , 1);
+                //draw squares
                 gl::drawSolidRect(curSquare);
             }
         }
-        //do all the things to find the squares -
-        
-
-        
-        
-        //divide sum by the appropriate numbers
-        //use the result from above to change color
-        //draw squares
     }
-    
-
 };
 
 
@@ -131,6 +175,7 @@ class SquaresFrameDiff : public Squares
 {
 private:
     cv::Mat frameDiff;
+    float sum = 0.0;
 public:
     virtual void drawRect(cv::Mat b)
     {
@@ -140,13 +185,20 @@ public:
     }
     virtual float count(ci::Rectf curSquare)
     {
-        //return the sum from the framedifferencing
-        return 0;
+        //counting white pixels
+        for(int x = x1 ; x < x2 ; x++){
+            for(int y = y1 ; y < y2 ; y++){
+                int pixel = frameDiff.at<uint8_t>(y,x);
+                sum+=pixel;
+            }
+        }
+        return sum;
     }
     
     float getDivisorOfSum()
     {
-        return 1;
+        float div = n*n*255;
+        return div;
     }
     
 
@@ -179,7 +231,7 @@ class SquaresFeatures : public Squares
     
     float getDivisorOfSum()
     {
-        return 1; //return features size class variable 
+        return 1; //return features size class variable
     }
     
 };
